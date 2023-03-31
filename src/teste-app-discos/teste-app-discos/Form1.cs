a9979634c4eed5a59c6dcfb3d6c962a1bd4e21d3
Carlos.Silva <carlos.silva@terceirizados.celcoin.com.br>
2023-03-31 17:18:20 -0300

    subindo setup da aplicacao e gitignore

---
+++ src/teste-app-discos/teste-app-discos/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace teste_app_discos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=NB-LIS-GY78KN3\SQLEXPRESS;Database=TESTES;Trusted_Connection=True;";
+            SqlConnection con = new SqlConnection(connectionString);
+
+            try
+            {             
+                con.Open();
+
+                SqlCommand command = new SqlCommand("SELECT NomeArtista FROM Artista", con);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader.GetString(0));
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            finally
+            {
+                if (con.State != 0)
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
